92078ed87a9353fd4613e9b19de82ce5db8ad20f
david-commit <davidondiege@gmail.com>
2023-08-10 17:13:31 +0300

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import { Settings } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+import { theme } from './theme';
 
 function App() {
-
   return (
     <>
-     hello world
+      <Button variant='outlined' startIcon={<Settings />}>
+        Hello World
+      </Button>
+      <Button
+        variant='contained'
+        startIcon={<Settings />}
+        sx={{
+          backgroundColor: 'purple',
+          '&:hover': {
+            backgroundColor: 'secondary',
+            padding: '5px 2rem',
+          },
+        }}
+      >
+        Hello World
+      </Button>
+      <br />
+      <Typography variant='h1' component='p' color='initial'>
+        Hello
+      </Typography>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
