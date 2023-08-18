2825ef92b888f3890777a84b235557c029de6a85
david-commit <davidondiege@gmail.com>
2023-08-18 21:52:49 +0300

    minor changes

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,51 @@
-import { Stack } from "@mui/material"
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Stack, Typography } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 
 const Feed = () => {
-  return <Stack flex={4} sx={{ border: '1px solid red' }}></Stack>;
-}
+  return (
+    <Stack flex={4} sx={{ border: '1px solid red', height: '100%' }}>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label='settings'>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title='Shrimp and Chorizo Paella'
+          subheader='September 14, 2016'
+        />
+        <CardMedia
+          component='img'
+          height='20%'
+          image='src/images/justin-groep-gRj8fOks0eg-unsplash.jpg'
+          alt='Paella dish'
+        />
+        <CardContent>
+          <Typography variant='body2' color='text.secondary'>
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label='add to favorites'>
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label='share'>
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
